e7b38577de30cfe2c85a1b94696a7e07b9951a04
MohamedManaa <manaamanaamohamedmanaamanaa@gmail.com>
2026-05-09 17:49:31 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ btn.onclick = function () {
   let h1 = document.querySelector("h1");
   //   h1.innerText = "صلي علي محمد";
   //   let h1Style = document.getAttribute("style");
-  if (btn.innerText == "خد حسنه") {
+  if (btn.innerText == "اضغط و خد حسنه") {
     h1.innerText = "صلى على النبى";
     btn.innerText = "خد حسنه تاني";
   } else if (btn.innerText == "خد حسنه تاني") {
     h1.innerText = "اذكر الله";
-    btn.innerText = "خد حسنه";
+    btn.innerText = "اضغط و خد حسنه";
   }
 };
